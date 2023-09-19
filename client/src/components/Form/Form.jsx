612318d4b02f12e49e5656aa9bbf05dacd3fd2c0
Fernando Aramayo <tripkefm@gmail.com>
2023-09-19 04:08:54 -0300

    Form permite agregar mas de un pais

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -11,7 +11,8 @@ export default function Form() {
   const navigate = useNavigate();
   const countries = useSelector((state) => state.allCountries);
   countries.sort((a,b)=>a.name.charCodeAt()- b.name.charCodeAt());
-  const [errors,setErrors]= useState({});
+  
+  const [errors, setErrors]= useState({});
 
   const [input, setInput]= useState({
     name: "",
@@ -27,22 +28,39 @@ export default function Form() {
   }, [dispatch])
 
   const handlerInputChange = (event) => {
-    setInput({
-      ...input,
-      [event.target.name] : event.target.value
-  });
-  setErrors(
+    setErrors(
       validate({
         ...input,
         [event.target.name]: event.target.value,
-      }));
+      })
+    );
+    setInput({
+      ...input,
+      [event.target.name] : event.target.value
+    });
   }
 
   const handlerSelectChange = (event) => { // para la opcion de multiples paises
-    setInput({
-      ...input,
-      countryId :[...input.countryId,event.target.value]
-  });
+    if (event.target.value === "") {
+      setErrors({
+        ...errors,
+        countryId: "Debes elegir un país",
+      });
+      return;
+    }
+    setErrors({
+      ...errors,
+      countryId: "",
+    });
+    const countryExists = input.countryId.find(
+      (item) => item === event.target.innerText
+    );
+    if (!countryExists) {
+      setInput({
+        ...input,
+        countryId :[...input.countryId, event.target.value],
+      });
+    }
   }
 
   const onSubmit = (event) => {
@@ -51,7 +69,7 @@ export default function Form() {
       input.duration === "" ||
       input.difficulty === "" ||
       input.season === "" ||
-      input.countryId.length === 0) return alert('Debe llenar los campos');
+      input.countryId.length === 0) return alert("Debe completar todos los campos");
       dispatch(postActivity(input))
       setInput({
         name: "",
@@ -60,7 +78,7 @@ export default function Form() {
         season: "",
         countryId: []
       })
-      alert("Actividad creada")
+      alert("Actividad creada correctamente")
       navigate("/home")
   }
   
@@ -77,46 +95,89 @@ export default function Form() {
               name='name'
               placeholder='Nombre hasta 30 caracteres...'
               type='text'
+              value={input.name}
               onChange={handlerInputChange}
             />
-            <p>{errors.name}</p>
+            {errors.name && (
+              <p className={styles.p}>{errors.name}</p>
+            )}
+            
             <label htmlFor='difficulty'>Dificultad: </label>
-            <input 
-              name='difficulty' 
-              placeholder='solo num. entre 1 y 5'
-              type="number"
-              onChange={handlerInputChange}
-            />
-            <p>{errors.difficulty}</p>
+              <select onChange={handlerInputChange} name='difficulty'>
+                <option value={input.difficulty}>Seleccionar Dificultad</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+              </select> 
+            <p className={styles.p}>{errors.difficulty}</p>
+
             <label htmlFor='duration'>Duración en hs.: </label>
-            <input 
-              name='duration' 
-              placeholder='solo num. entre 1 y 24'
-              type="number"
-              onChange={handlerInputChange}
-            />
-            <p>{errors.duration}</p>
+            <select onChange={handlerInputChange} name='duration'>
+                <option value={input.duration}>Seleccionar Duración en hs</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+                <option value='8'>8</option>
+                <option value='9'>9</option>
+                <option value='10'>10</option>
+                <option value='11'>11</option>
+                <option value='12'>12</option>
+                <option value='13'>13</option>
+                <option value='14'>14</option>
+                <option value='15'>15</option>
+                <option value='16'>16</option>
+                <option value='17'>17</option>
+                <option value='18'>18</option>
+                <option value='19'>19</option>
+                <option value='20'>20</option>
+                <option value='21'>21</option>
+                <option value='22'>22</option>
+                <option value='23'>23</option>
+                <option value='24'>24</option>
+               </select> 
+            <p className={styles.p}>{errors.duration}</p>
+
             <label htmlFor='season'>Estaciones del año: </label>
               <select onChange={handlerInputChange} name='season'>
-                  <option value=''>Select season</option>
+                  <option value={input.season}>Seleccionar estación</option>
                   <option value='Spring'>Primavera</option>
                   <option value='Summer'>Verano</option>
                   <option value='Autumn'>Otoño</option>
                   <option value='Winter'>Invierno</option>
               </select>
-            <p>{errors.season}</p>
+            <p className={styles.p}>{errors.season}</p>
+
             <label htmlFor='countries'>Paises:</label>
-                <select
-                    onChange={handlerSelectChange}
-                    name='countries'>                
+                <select onChange={handlerSelectChange} name='countries'>                
                     <option value=''>Seleccionar Paises</option>
-                    {   //mapeamos el arreglo de paises para obtener el nombre en los select
-                        countries.map((country) => <option key={country.id} value={country.id}>
-                            {country.name}</option>
-                        )
+                    {countries && 
+                      countries.map((country) => { 
+                        return (
+                          <option key={country.id} value={country.id}>
+                            {country.name}
+                          </option>
+                          );
+                        })
                     }
                 </select>
-                <p>{errors.countries}</p>
+                <input
+                  name='countries'
+                  type='text'
+                  value={input.countryId}
+                  onChange={handlerInputChange}
+                  placeholder='Seleccione al menos un país'
+
+                />
+                {errors.countryId && (
+                  <p className={styles.p}>{errors.countryId}</p>
+                )}
+                
                 <button className={styles.createButton} type='submit'>Crear Actividad</button>
           </form>
         </div>
--- client/src/components/Form/validate.js
+++ client/src/components/Form/validate.js
@@ -1,32 +1,28 @@
-function validate(activity) {
-    const patterns = {
-      name: {
-        pattern: /^\S[a-zA-Z\s]{1,20}\S$/,
-        errorMessage: "El nombre debe ser con letras de A la Z y desde 3 hasta 30 caracteres",
-      },
-      difficulty: {
-        pattern: /^(1|2|3|4|5)$/,
-        errorMessage:
-          "La dificultad debe ser igual o mayor 1, o igual o menor a 5",
-      },
-      duration: {
-        pattern: /^(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24)$/,
-        errorMessage: "La duración en horas debe ser igual o mayor 1, o igual o menor a 24",
-      },
-      season: {
-        pattern: /^(Spring|Summer|Autumn|Winter)$/,
-        errorMessage: "Debes seleccionar al menos una estación del año",
-      },
-    };
-  
-    let errorActivity = {};
-  
-    for (const error in patterns) {
-      if (!patterns[error].pattern.test(activity[error])) {
-        errorActivity[error] = patterns[error].errorMessage;
-      }
-    };
-      return errorActivity;
-  };
+function validate(input) {
+  let errors = {};
 
-  export default validate;
+  if(!input.name) {
+    errors.name = "Debe ingresar un nombre para la actividad";
+  } else if (!/^\S[a-zA-Z\s]{1,20}\S$/.test(input.name)) {
+    errors.name = "El nombre debe ser con letras de A la Z y desde 3 hasta 30 caracteres";
+  }
+    
+  if (!input.difficulty) {
+    errors.difficulty = "Debe seleccionar una dificultad";
+  }
+
+  if (!input.duration) {
+    errors.duration = "Debe seleccionar una duración en hs";
+  }
+
+  if (!input.season) {
+    errors.season = "Debes seleccionar al menos una estación del año";
+  }
+
+  if (!input.countries) {
+    errors.countries = "Debes seleccionar al menos un ispa";
+  }
+  return errors;
+};
+
+export default validate;
