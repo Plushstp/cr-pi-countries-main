f535cc9480ae59d8a5bcc7ea3a18cc2bffc2a175
Fernando Aramayo <tripkefm@gmail.com>
2023-09-19 21:53:47 -0300

    Listo Searchbar, ya retorna mensaje cuando no encuentra pais

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,31 +1,45 @@
 import React from "react";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { getCountryByName } from "../../redux/actions"
+import { useDispatch, useSelector } from "react-redux";
+import { getCountryByName, getAllCountries } from "../../redux/actions"
 import style from './SearchBar.module.css'
 
 
 export default function SearchBar (){
     const dispatch = useDispatch();
-    const [name, setName] = useState("");
+    const [search, setSearch] = useState("");
+    const countries = useSelector((state) => state.allCountries);
        
     function handleInputChange (event){
         event.preventDefault();
-        setName(event.target.value);
+        setSearch(event.target.value);
         
-        console.log(name);
+        console.log(search);
     };
 
     function handleSubmit (event){
         event.preventDefault();
-        if (name.length === 0) return alert('Debe ingresar un País en el cuadro de búsqueda');
-        dispatch(getCountryByName(name));
-        setName('');
+        if (search.length === 0) return alert('Debe ingresar un País en el cuadro de búsqueda');
+        const toSearch = search.toLowerCase();
+        const validate = countries.filter((event) => 
+            event.name.toLowerCase().includes(toSearch)
+        );
+        if (validate.length < 1) { 
+         return alert('No existe pais con ese nombre');
+        } else { 
+        dispatch(getCountryByName(search))
+        
+        }
+        setSearch('');
     }
 
     return(
         <div>
-            <input className={style.inputSearch} type='text' value={name} placeholder="Nombre de país..." onChange={(event) => handleInputChange(event)} required/>
+            <input className={style.inputSearch} 
+            type='text' 
+            value={search} 
+            placeholder={"Ingresar nombre de país"}
+            onChange={(event) => handleInputChange(event)} required/>
 
             <button className={style.buttonNav} type="submit" value="" onClick={(event) => handleSubmit(event)}>Buscar</button>
         </div>
